63f2fd5190de9c4ac3e6de40642d2e942ef1727e
Nishita <nishitagupta2005@gmail.com>
2025-07-09 16:06:07 +0530

    initial commit-To Do list

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysql = require('mysql2');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'HN15',
+    database: 'todo_list'
+});
+
+db.connect((err) =>{
+    if (err) 
+        throw err;
+    console.log("Connected To Database");
+});
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('public'));
+app.set('view engine','ejs');
+
+app.get('/',(req,res) => {
+    db.query("SELECT * FROM todos",(err,results) => {
+        if (err)
+            throw err;
+        res.render('index',{todos:results});
+    });
+});
+
+app.post('/add',(req,res) => {
+    const task = req.body.task;
+    db.query("INSERT INTO todos (task) VALUES (?) " ,[task], (err) => {
+        if (err)
+            throw err;
+        res.redirect('/');
+    });
+});
+
+app.post('/delete/:id',(req,res) =>{
+    const id = req.params.id;
+    db.query("DELETE FROM todos WHERE id = ?", [id],(err) =>{
+        if (err)
+            throw err;
+        res.redirect('/');
+    });
+});
+
+app.post('/edit/:id',(req,res) =>{
+    const updatedTask = req.body.updatedTask;
+    const id = req.params.id;
+    db.query("UPDATE todos SET task = ? WHERE id = ?",[updatedTask,id], (err) =>{
+        if (err)
+            throw err;
+        res.redirect('/')
+    });
+});
+
+app.post('/toggle/:id', (req, res) => {
+  const id = req.params.id;
+  db.query("UPDATE todos SET completed = NOT completed WHERE id = ?", [id], (err) => {
+    if (err) throw err;
+    res.redirect('/');
+  });
+});
+
+
+app.listen(PORT, () =>{
+    console.log(`Server running at http://localhost:${PORT}`)
+});
